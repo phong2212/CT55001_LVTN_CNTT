09effd4ebc48ed20db3773b3dde4be25e6b958f4
phong2212 <phongb2014685@student.ctu.edu.vn>
2024-12-10 09:54:57 +0700

    Cập nhật NextRequest

--- next-app/app/api/availability/route.ts
+++ next-app/app/api/availability/route.ts
@@ -1,21 +1,21 @@
 import prisma from "@/app/utils/connect";
-import { auth } from '@clerk/nextjs/server';
-import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
     try {
-        const url = new URL(req.url);
-        const page = parseInt(url.searchParams.get('page') || '1', 10);
-        const limit = parseInt(url.searchParams.get('limit') || '6', 10);
+        const url = req.nextUrl;
+        const page = parseInt(url.searchParams.get("page") || "1", 10);
+        const limit = parseInt(url.searchParams.get("limit") || "6", 10);
         const skip = (page - 1) * limit;
-        const filter = url.searchParams.get('filter') || 'all';
+        const filter = url.searchParams.get("filter") || "all";
 
-        const whereClause = filter === 'all' ? {} : { available: filter === 'true' };
+        const whereClause = filter === "all" ? {} : { available: filter === "true" };
 
         const available = await prisma.availability.findMany({
             where: whereClause,
             orderBy: {
-                id: 'desc',
+                id: "desc",
             },
             take: limit,
             skip: skip,
@@ -26,10 +26,10 @@ export async function GET(req: Request) {
         });
 
         const all = await prisma.availability.findMany({});
-        
-        return NextResponse.json({ all, available, total,  status: 200 });
+
+        return NextResponse.json({ all, available, total, status: 200 });
     } catch (error) {
-        console.log("Lỗi lấy tình trạng phòng: ", error);
+        console.error("Lỗi lấy tình trạng phòng: ", error);
         return NextResponse.json({ error: "Lỗi lấy tình trạng phòng", status: 500 });
     }
 }
@@ -37,25 +37,22 @@ export async function GET(req: Request) {
 export async function PUT(req: Request) {
     try {
         const { userId } = auth();
-        const { id , available } = await req.json();
+        const { id, available } = await req.json();
 
         if (!userId) {
             return NextResponse.json({ error: "Không có quyền truy cập", status: 401 });
         }
 
-        const avaiable = await prisma.availability.update({
+        const updatedAvailability = await prisma.availability.update({
             where: { id },
             data: {
                 available,
-            }
+            },
         });
 
-        return NextResponse.json({ avaiable, status: 200 });
+        return NextResponse.json({ updatedAvailability, status: 200 });
     } catch (error) {
-        console.log("Lỗi cập nhật tình trạng phòng: ", error);
+        console.error("Lỗi cập nhật tình trạng phòng: ", error);
         return NextResponse.json({ error: "Lỗi cập nhật tình trạng phòng", status: 500 });
     }
 }
-
-
-
